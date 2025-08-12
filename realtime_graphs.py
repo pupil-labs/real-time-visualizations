bad06b440da3b27ed0520751cd446e38aadc24d3
Robert Ennis <rob@pupil-labs.com>
2025-08-12 12:14:29 +0200

    separate out & simplify RT examples

---
+++ realtime_graphs.py
@@ -0,0 +1,187 @@
+import threading
+import time
+from queue import Queue
+
+import cv2
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+from pupil_labs.realtime_api.simple import Device
+from pupil_labs.realtime_api.streaming.eye_events import (
+    BlinkEventData,
+)
+
+device = Device(address="192.168.1.57", port=8080)
+# device = Device(address="172.20.10.2", port=8080)
+
+
+# Setup plot
+fig, (ax_eye_image, ax_blinks, ax_pupil_diameter) = plt.subplots(
+    3, 1, figsize=(8, 8), gridspec_kw={"height_ratios": [2, 1, 1]}
+)
+fig.canvas.manager.set_window_title("3D Eye State Visualization")
+fig.patch.set_facecolor("black")
+
+# Eye image subplot
+ax_eye_image.axis("off")
+eye_image_display = ax_eye_image.imshow(
+    np.zeros((192, int((192 * 2) * 1.2), 3), dtype=np.uint8)
+)  # placeholder
+
+# Blinks subplot
+ax_blinks.set_facecolor("black")
+ax_blinks.set_xlim(0, 100)
+ax_blinks.set_ylim(0, 1.3)
+ax_blinks.set_xticklabels([])
+ax_blinks.set_yticklabels([])
+ax_blinks.grid(False)
+ax_blinks.text(
+    0.05,
+    0.95,
+    "Blinks",
+    color="white",
+    fontsize=10,
+    transform=ax_blinks.transAxes,
+    va="top",
+)
+ax_blinks.tick_params(colors="white")
+for spine in ax_blinks.spines.values():
+    spine.set_edgecolor("white")
+
+(plot_blinks,) = ax_blinks.plot([], [], linewidth=1.5)
+plot_blinks.set_color((99 / 255, 108 / 255, 191 / 255))
+
+# Pupil diameter subplot
+ax_pupil_diameter.set_facecolor("black")
+ax_pupil_diameter.set_xlim(0, 100)
+ax_pupil_diameter.set_ylim(0, 8.0)
+ax_pupil_diameter.set_xticklabels([])
+ax_pupil_diameter.set_yticklabels([])
+ax_pupil_diameter.grid(False)
+ax_pupil_diameter.text(
+    0.02,
+    0.90,
+    "Pupil Diameter [mm]",
+    color="white",
+    fontsize=10,
+    transform=ax_pupil_diameter.transAxes,
+    va="top",
+)
+ax_pupil_diameter.tick_params(colors="white")
+for spine in ax_pupil_diameter.spines.values():
+    spine.set_edgecolor("white")
+
+(plot_left_pupil,) = ax_pupil_diameter.plot([], [], linewidth=1.5)
+plot_left_pupil.set_color((101 / 255, 188 / 255, 118 / 255))
+
+(plot_right_pupil,) = ax_pupil_diameter.plot([], [], linewidth=1.5)
+plot_right_pupil.set_color((229 / 255, 111 / 255, 114 / 255))
+
+
+data_queue = Queue(maxsize=1)
+
+
+def data_acquisition_loop():
+    while True:
+        eye_image = device.receive_eyes_video_frame(timeout_seconds=0.01)
+        gaze = device.receive_gaze_datum(timeout_seconds=0.01)
+        eye_event = device.receive_eye_events(timeout_seconds=0.01)
+
+        if gaze is not None and eye_image is not None and eye_event is not None:
+            eye_image = eye_image.bgr_pixels
+
+            eye_image = cv2.resize(
+                eye_image, (int(eye_image.shape[1] * 1.2), eye_image.shape[0])
+            )
+
+            if data_queue.full():
+                data_queue.get_nowait()
+
+            data_queue.put_nowait(
+                {
+                    "eye": eye_image,
+                    "gaze": gaze,
+                    "eye_event": eye_event,
+                }
+            )
+
+
+data_acquisition_thread = threading.Thread(target=data_acquisition_loop, daemon=True)
+data_acquisition_thread.start()
+
+blink_data = []
+blinked = False
+blink_time = 0.0
+
+left_pupil_data = []
+right_pupil_data = []
+
+
+def update(frame_number):
+    global blink_data
+    global blinked
+    global blink_time
+
+    global left_pupil_data
+    global right_pupil_data
+
+    frame = None
+    eye_event = None
+    if not data_queue.empty():
+        data = data_queue.get()
+        frame = data["eye"]
+        gaze = data["gaze"]
+        eye_event = data["eye_event"]
+
+    if frame is not None:
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            cv2.destroyAllWindows()
+            return
+
+        eye_image_display.set_data(frame)
+
+    if isinstance(eye_event, BlinkEventData):
+        blink_time = time.time_ns()
+        blinked = True
+
+    if blinked:
+        y = 0.8
+        if time.time_ns() - blink_time > (0.1 * 1e9):
+            blinked = False
+    else:
+        y = 0.0
+
+    blink_data.append(y)
+    if len(blink_data) > 100:
+        blink_data = blink_data[-100:]
+
+    x_vals = np.arange(len(blink_data))
+    plot_blinks.set_data(x_vals, blink_data)
+
+    pupil_diameter_left = 0.0
+    pupil_diameter_right = 0.0
+    if gaze is not None:
+        pupil_diameter_left = gaze.pupil_diameter_left
+        pupil_diameter_right = gaze.pupil_diameter_right
+
+        left_pupil_data.append(pupil_diameter_left)
+        if len(left_pupil_data) > 100:
+            left_pupil_data = left_pupil_data[-100:]
+
+        x_vals = np.arange(len(left_pupil_data))
+        plot_left_pupil.set_data(x_vals, left_pupil_data)
+
+        right_pupil_data.append(pupil_diameter_right)
+        if len(right_pupil_data) > 100:
+            right_pupil_data = right_pupil_data[-100:]
+
+        x_vals = np.arange(len(right_pupil_data))
+        plot_right_pupil.set_data(x_vals, right_pupil_data)
+
+    return [plot_blinks] + [eye_image_display] + [plot_left_pupil] + [plot_right_pupil]
+
+
+ani = animation.FuncAnimation(fig, update, interval=33, blit=False)
+plt.tight_layout()
+plt.show()
--- realtime_imu.py
+++ realtime_imu.py
@@ -8,31 +8,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from pupil_labs.realtime_api.simple import Device
 from scipy.spatial.transform import Rotation as R
 
-
-def transform_imu_to_world(imu_coordinates, imu_quaternions):
-    # This array contains a timeseries of transformation matrices,
-    # as calculated from the IMU's timeseries of quaternions values.
-    imu_to_world_matrices = R.from_quat(imu_quaternions).as_matrix()
-
-    if np.ndim(imu_coordinates) == 1:
-        return imu_to_world_matrices @ imu_coordinates
-    else:
-        return np.array(
-            [
-                imu_to_world @ imu_coord
-                for imu_to_world, imu_coord in zip(
-                    imu_to_world_matrices, imu_coordinates
-                )
-            ]
-        )
-
-
-def imu_heading_in_world(imu_quaternions):
-    heading_neutral_in_imu_coords = np.array([0.0, 1.0, 0.0])
-    return transform_imu_to_world(heading_neutral_in_imu_coords, imu_quaternions)
-
-
-device = Device(address="192.168.1.35", port=8080)
+device = Device(address="192.168.1.57", port=8080)
 
 imu_queue = Queue(maxsize=1)
 
@@ -101,78 +77,6 @@ ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
 
-ax2 = fig.add_subplot(132)
-
-ax2.axis("square")
-ax2.set_aspect("equal", adjustable="box")
-ax2.set_xlim(-1.29, 1.29)
-ax2.set_ylim(-1.29, 1.29)
-
-ax2.axis("off")
-ax2.set_facecolor("black")
-ax2.set_xticks([])
-ax2.set_yticks([])
-
-circle = plt.Circle((0, 0), 1.0, color="white", fill=False)
-ax2.add_patch(circle)
-
-ax2.text(0, 1.11, "N", ha="center", va="bottom", fontsize="xx-large", color="white")
-ax2.text(0, -1.11, "S", ha="center", va="top", fontsize="xx-large", color="white")
-ax2.text(1.11, 0, "E", ha="left", va="center", fontsize="xx-large", color="white")
-ax2.text(-1.11, 0, "W", ha="right", va="center", fontsize="xx-large", color="white")
-
-ax2.plot([0, 0], [1.0, 1.08], color="white")
-ax2.plot([0, 0], [-1.0, -1.08], color="white")
-ax2.plot([1.0, 1.08], [0, 0], color="white")
-ax2.plot([-1.0, -1.08], [0, 0], color="white")
-
-nsew_plot = ax2.quiver(
-    0,
-    0,
-    0,
-    0,
-    color="b",
-    scale=1,
-    scale_units="xy",
-    angles="xy",
-    width=0.01,
-)
-
-ax3 = fig.add_subplot(133)
-
-ax3.axis("square")
-ax3.set_aspect("equal", adjustable="box")
-ax3.set_xlim(-1.29, 1.29)
-ax3.set_ylim(-1.29, 1.29)
-
-ax3.axis("off")
-ax3.set_facecolor("black")
-ax3.set_xticks([])
-ax3.set_yticks([])
-
-circle = plt.Circle((0, 0), 1.0, color="white", fill=False)
-ax3.add_patch(circle)
-
-ax3.plot([0, 0], [1.0, 1.08], color="white")
-ax3.plot([0, 0], [-1.0, -1.08], color="white")
-
-ax3.text(0, 1.11, "Sky", ha="center", va="bottom", fontsize="xx-large", color="white")
-ax3.text(0, -1.11, "Earth", ha="center", va="top", fontsize="xx-large", color="white")
-
-ax3.hlines(0, -1.0, 1.0, color="white", linestyle="--")
-
-earth_sky_plot = ax3.quiver(
-    0,
-    0,
-    0,
-    0,
-    color="b",
-    scale=1,
-    scale_units="xy",
-    angles="xy",
-    width=0.01,
-)
-
 
 def update(frame):
     if not imu_queue.empty():
@@ -184,42 +88,6 @@ def update(frame):
         rotated = original_vertices @ rotation_matrix.T
         new_mesh = [[rotated[idx] for idx in face] for face in faces]
         poly3d.set_verts(new_mesh)
-
-        heading_in_world = imu_heading_in_world(quat)
-
-        nsew_heading = np.array([heading_in_world[0], heading_in_world[1]])
-        nsew_heading /= np.linalg.norm(nsew_heading)
-
-        global nsew_plot
-        nsew_plot.remove()
-        nsew_plot = ax2.quiver(
-            0,
-            0,
-            nsew_heading[0],
-            nsew_heading[1],
-            color="b",
-            scale=1,
-            scale_units="xy",
-            angles="xy",
-            width=0.01,
-        )
-
-        earth_sky_heading = np.array([heading_in_world[1], heading_in_world[2]])
-        earth_sky_heading /= np.linalg.norm(earth_sky_heading)
-
-        global earth_sky_plot
-        earth_sky_plot.remove()
-        earth_sky_plot = ax3.quiver(
-            0,
-            0,
-            earth_sky_heading[0],
-            earth_sky_heading[1],
-            color="b",
-            scale=1,
-            scale_units="xy",
-            angles="xy",
-            width=0.01,
-        )
 
         return (poly3d,)
 
